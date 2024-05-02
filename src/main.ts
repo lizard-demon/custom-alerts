6402d563ec3c226cf8d01704b2f8f84697236c11
randy-goodman <randy.goodman@reddit.com>
2024-05-02 11:51:20 -0400

    Update

--- src/main.ts
+++ src/main.ts
@@ -1,124 +1,123 @@
-import { Devvit, SubredditContextActionEvent, Context, RedditAPIClient, getFromMetadata, Header } from '@devvit/public-api';
-import { InternalDevvit } from '@devvit/public-api/abstractions/InternalDevvit.js'
+import { Devvit, MenuItem, Context, MenuItemOnPressEvent } from '@devvit/public-api';
 import { Metadata, ContextActionResponse, KeySet, MessageSet, ListFilter, PostSubmit, PostV2 } from '@devvit/protos';
 
-const kvstore = Devvit.use(Devvit.Types.KVStore);
-const reddit = new RedditAPIClient();
+Devvit.configure({
+    redditAPI: true,
+    kvStore: true,
+});
 
-async function getSubscribed(): Promise<string[]> {
-    const data = await kvstore.List(ListFilter.fromPartial({
-        filter: "*",
-    }));
+async function getSubscribed(context: Context): Promise<string[]> {
+    const subscribed = await context.redis.hgetall("subscribed");
+    if(!subscribed) {
+        return [];
+    }
 
-    return data.keys;
+    return Object.keys(subscribed);
 }
 
-async function addSubscription(user: string): Promise<void> {
-    await kvstore.Put(MessageSet.fromPartial({
-        messages: {
-            [user]: "",
-        }
-    }));
+async function addSubscription(context: Context, user: string): Promise<void> {
+    await context.redis.hset("subscribed", { [user]: "" });
 }
 
-async function deleteSubscription(user: string): Promise<void> {
-    await kvstore.Del(KeySet.fromPartial({
-        keys: [user],
-    }));
+async function deleteSubscription(context: Context, user: string): Promise<void> {
+    await context.redis.hdel("subscribed", [user]);
 }
 
-async function subscribeHandler(_: SubredditContextActionEvent, metadata?: Metadata | undefined): Promise<ContextActionResponse> {
-    const userID = getFromMetadata(Header.User, metadata);
-    if(userID === undefined) {
-        return ContextActionResponse.fromPartial({
-            success: false,
-            message: "You must be logged in to use this app."
+async function subscribeHandler(event: MenuItemOnPressEvent, context: Devvit.Context): Promise<void> {
+    const userID = context.userId;
+    if(!userID) {
+        context.ui.showToast({
+            appearance: 'neutral',
+            text: "You must be logged in to use this app."
         });
     }
 
-    const user = await reddit.getUserById(userID, metadata);
-    if(user === undefined) {
-        return ContextActionResponse.fromPartial({
-            success: false,
-            message: "Couldn't fetch user."
-        }); 
+    const user = await context.reddit.getUserById(userID);
+    if(!user) {
+        context.ui.showToast({
+            appearance: 'neutral',
+            text: "Couldn't fetch user."
+        });
     }
 
-    await addSubscription(user.username.toLowerCase());
-
-    return ContextActionResponse.fromPartial({
-        success: true,
-        message: "You have subscribed to new posts from this subreddit."
+    await addSubscription(context, user.username.toLowerCase());
+    context.ui.showToast({
+        appearance: 'success',
+        text: "You have subscribed to new posts from this subreddit."
     });
 }
 
-async function unsubscribeHandler(_: SubredditContextActionEvent, metadata?: Metadata | undefined): Promise<ContextActionResponse> {
-    const userID = getFromMetadata(Header.User, metadata);
-    if(userID === undefined) {
-        return ContextActionResponse.fromPartial({
-            success: false,
-            message: "You must be logged in to use this app."
+async function unsubscribeHandler(event: MenuItemOnPressEvent, context: Devvit.Context): Promise<void> {
+    const userID = context.userId;
+    if(!userID) {
+        context.ui.showToast({
+            appearance: 'neutral',
+            text: "You must be logged in to use this app."
         });
     }
 
-    const user = await reddit.getUserById(userID, metadata);
-    if(user === undefined) {
-        return ContextActionResponse.fromPartial({
-            success: false,
-            message: "Couldn't fetch user."
-        }); 
+    const user = await context.reddit.getUserById(userID);
+    if(!user) {
+        context.ui.showToast({
+            appearance: 'neutral',
+            text: "Couldn't fetch user."
+        });
     }
 
-    await deleteSubscription(user.username.toLowerCase());
-
-    return ContextActionResponse.fromPartial({
-        success: true,
-        message: "You will no longer receive notifications about new posts from this subreddit."
-    })
+    await deleteSubscription(context, user.username.toLowerCase());
+    context.ui.showToast({
+        appearance: 'success',
+        text: "You will no longer receive notifications about new posts from this subreddit."
+    });
 }
 
-Devvit.addActions([
+const menuItems: MenuItem[] = [
     {
-        context: Context.SUBREDDIT,
-        handler: subscribeHandler,
-        name: "Get Notified for New Posts",
+        location: "subreddit",
+        onPress: subscribeHandler,
+        label: "Get Notified for New Posts",
         description: "Receive notifications about new posts in this subreddit."
     },
     {
-        context: Context.SUBREDDIT,
-        handler: unsubscribeHandler,
-        name: "Stop Getting New Post Notifications",
+        location: "subreddit",
+        onPress: unsubscribeHandler,
+        label: "Stop Getting New Post Notifications",
         description: "Stop receiving notifications about new posts in this subreddit."
     }
-]);
+];
 
-async function generateMessageBody(post: PostV2, url: string, metadata: Metadata | undefined): Promise<string> {
-    const author = await reddit.getUserById(post.authorId, metadata);
+menuItems.forEach((action) => {
+    Devvit.addMenuItem(action);
+});
+
+async function generateMessageBody(context: Context, post: PostV2, url: string): Promise<string> {
+    const author = await context.reddit.getUserById(post.authorId);
 
     return `Author: u/${author.username}\n\nTitle: ${post.title}\n\n________\n\n${url}`  
 }
 
-InternalDevvit.onPostSubmit(async (postSubmit: PostSubmit, metdata?: Metadata | undefined): Promise<{}> => {
-    const subscribed = await getSubscribed();
-    if(postSubmit.post === undefined) {
-        return {};
+Devvit.addTrigger({
+    event: 'PostCreate',
+    async onEvent(postSubmit: PostSubmit, context: Context): Promise<void> {
+        const subscribed = await getSubscribed(context);
+        if(postSubmit.post === undefined) {
+            return;
+        }
+
+        const postId = postSubmit.post.id.substring(3);
+        const subreddit = await context.reddit.getSubredditById(postSubmit.post.subredditId);
+        const subredditName = subreddit.name;
+        const url = `https://www.reddit.com/r/${subredditName}/comments/${postId}/_/`;
+        const body = await generateMessageBody(context, postSubmit.post, url);
+
+        subscribed.forEach((username) => {
+            context.reddit.sendPrivateMessage({
+                to: username,
+                subject: `A new post has been made in r/${subredditName}`,
+                text: body,
+            });
+        });
     }
-
-    const postId = postSubmit.post.id.substring(3);
-    const subreddit = await reddit.getSubredditById(postSubmit.post.subredditId, metdata);
-    const subredditName = subreddit.name;
-    const url = `https://www.reddit.com/r/${subredditName}/comments/${postId}/_/`;
-    const body = await generateMessageBody(postSubmit.post, url, metdata);
-
-    subscribed.forEach((username) => {
-        reddit.sendPrivateMessage({
-            to: username,
-            subject: `A new post has been made in r/${subredditName}`,
-            text: body,
-        }, metdata);
-    });
-
-    return {};
-})
+});
 
 export default Devvit;
